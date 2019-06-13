63982daf114e18699b3de719600bf4164f7b65b4
pamellasds <48889578+pamellasds@users.noreply.github.com>
2019-06-12 22:37:08 -0300

    Add files via upload

--- 3opt_entrada.py
+++ 3opt_entrada.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 import numpy as np
 import random
+import operator
+import itertools
 
 def entrada(nome_arq):
     #Abrir o Arquivo
@@ -53,9 +55,6 @@ def entrada(nome_arq):
             split = texto.split()
             for j in range(0, N):
                 cidades[i,j] = int(split[j])
-
-    
-    
     arq.close()
 
     return cidades
@@ -72,20 +71,57 @@ def custo_caminho_inicial(matriz_cidades):
 
 def escolher_arestas_iniciais(matriz_cidades):
     vertice_x_y_z=[]
-    vertice_a_b_c = random.sample(range(0,matriz_cidades.shape[0]-1), 3) #escolhendo três (n) vértices aleatórios
+    vertice_a_b_c=[]
+    vertice = random.randrange(0,matriz_cidades.shape[0], 2)
+    vertice_a_b_c.append(vertice)
+    while len(vertice_a_b_c)<3:
+        vertice = random.randrange(0,matriz_cidades.shape[0], 2) #escolhendo três (n) vértices aleatórios
+        if not(vertice in vertice_a_b_c):
+            vertice_a_b_c.append(vertice)
     [vertice_x_y_z.append((vertice_a_b_c[i])+1) for i in range(0,3)] #encontrando os vertices próximos (n+1) para formar aresta
     return vertice_a_b_c, vertice_x_y_z 
 
-
-def gerar_combinacoes():
     
+def gerar_combinacoes(matriz_cidades, vertice_a_b_c_aux, vertice_x_y_z_aux):
+    distancia=[]
+    arestas=[]
+
+    #arestas a serem trocadas
+    distancia.append(matriz_cidades[vertice_a_b_c_aux[0], vertice_x_y_z_aux[0]] + matriz_cidades[vertice_a_b_c_aux[1], vertice_x_y_z_aux[1]] + matriz_cidades[vertice_a_b_c_aux[2], vertice_x_y_z_aux[2]])
+    arestas.append([[vertice_a_b_c_aux[0], vertice_x_y_z_aux[0]], [vertice_a_b_c_aux[1], vertice_x_y_z_aux[1]], [vertice_a_b_c_aux[2], vertice_x_y_z_aux[2]]])
+    #primeira permutação
+    distancia.append(matriz_cidades[vertice_a_b_c_aux[0], vertice_x_y_z_aux[2]] + matriz_cidades[vertice_a_b_c_aux[2], vertice_x_y_z_aux[0]] + matriz_cidades[vertice_a_b_c_aux[1], vertice_x_y_z_aux[1]])
+    arestas.append([[vertice_a_b_c_aux[0], vertice_x_y_z_aux[2]], [vertice_a_b_c_aux[2], vertice_x_y_z_aux[0]], [vertice_a_b_c_aux[1], vertice_x_y_z_aux[1]]])
+    #segunda permutação
+    distancia.append(matriz_cidades[vertice_a_b_c_aux[0], vertice_x_y_z_aux[0]] + matriz_cidades[vertice_a_b_c_aux[1], vertice_x_y_z_aux[2]] + matriz_cidades[vertice_a_b_c_aux[2], vertice_x_y_z_aux[1]])
+    arestas.append([[vertice_a_b_c_aux[0], vertice_x_y_z_aux[0]], [vertice_a_b_c_aux[1], vertice_x_y_z_aux[2]], [vertice_a_b_c_aux[2], vertice_x_y_z_aux[1]]])
+    #terceira permutação
+    distancia.append(matriz_cidades[vertice_a_b_c_aux[0], vertice_a_b_c_aux[1]] + matriz_cidades[vertice_x_y_z_aux[0], vertice_x_y_z_aux[1]] + matriz_cidades[vertice_a_b_c_aux[2], vertice_x_y_z_aux[2]])
+    arestas.append([[vertice_a_b_c_aux[0], vertice_x_y_z_aux[1]], [vertice_a_b_c_aux[0], vertice_x_y_z_aux[1]], [vertice_a_b_c_aux[2], vertice_x_y_z_aux[2]]])
+    #quarta permutação
+    distancia.append(matriz_cidades[vertice_a_b_c_aux[0], vertice_a_b_c_aux[1]] + matriz_cidades[vertice_x_y_z_aux[2], vertice_x_y_z_aux[1]] + matriz_cidades[vertice_a_b_c_aux[2], vertice_x_y_z_aux[0]])
+    arestas.append([[vertice_a_b_c_aux[0], vertice_x_y_z_aux[1]], [vertice_a_b_c_aux[2], vertice_x_y_z_aux[1]], [vertice_a_b_c_aux[2], vertice_x_y_z_aux[0]]])
+    #quinta permutação
+    distancia.append(matriz_cidades[vertice_a_b_c_aux[0], vertice_a_b_c_aux[2]] + matriz_cidades[vertice_x_y_z_aux[0], vertice_x_y_z_aux[1]] + matriz_cidades[vertice_x_y_z_aux[2], vertice_a_b_c_aux[1]])
+    arestas.append([[vertice_a_b_c_aux[0], vertice_a_b_c_aux[2]], [vertice_x_y_z_aux[0], vertice_x_y_z_aux[1]], [vertice_x_y_z_aux[2], vertice_a_b_c_aux[1]]])
+    #sexta permutação
+    distancia.append(matriz_cidades[vertice_a_b_c_aux[0], vertice_x_y_z_aux[1]] + matriz_cidades[vertice_x_y_z_aux[2], vertice_x_y_z_aux[0]] + matriz_cidades[vertice_a_b_c_aux[2], vertice_a_b_c_aux[2]])
+    arestas.append([[vertice_a_b_c_aux[0], vertice_x_y_z_aux[1]], [vertice_x_y_z_aux[2], vertice_x_y_z_aux[0]], [vertice_a_b_c_aux[2], vertice_a_b_c_aux[2]]])
+    #sétima permutação
+    distancia.append(matriz_cidades[vertice_a_b_c_aux[0], vertice_x_y_z_aux[1]] + matriz_cidades[vertice_x_y_z_aux[0], vertice_a_b_c_aux[2]] + matriz_cidades[vertice_a_b_c_aux[1], vertice_a_b_c_aux[2]])
+    arestas.append([[vertice_a_b_c_aux[0], vertice_x_y_z_aux[1]], [vertice_x_y_z_aux[0], vertice_a_b_c_aux[2]], [vertice_a_b_c_aux[1], vertice_a_b_c_aux[2]]])
+
+    return distancia, arestas
+
+def escolher_melhor_troca(arestas,distancia):
+    return arestas[distancia.index(min(distancia))], min(distancia)
 
 
-print(custo_caminho_inicial(entrada('datasets/tsp10t3.txt')))
-print(escolher_arestas_iniciais(entrada('datasets/tsp10t3.txt')))
 
-
-
-
-
+print(custo_caminho_inicial(entrada('datasets/tsp10t3_teste.txt')))
+vertice_a_b_c_aux, vertice_x_y_z_aux = escolher_arestas_iniciais(entrada('datasets/tsp10t3_teste.txt'))
+print(vertice_a_b_c_aux, vertice_x_y_z_aux)
+distancia, arestas = gerar_combinacoes(entrada('datasets/tsp10t3_teste.txt'), vertice_a_b_c_aux, vertice_x_y_z_aux)
+print(distancia, arestas)
+arestas_sub, melhor_distancia = escolher_melhor_troca(distancia, arestas)
 
